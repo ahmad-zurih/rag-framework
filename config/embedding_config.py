bc433c6c776cc68cbdf87aac10e5a80a587aeccf
Ahmad Alhineidi <ahmadhineidi@hotmail.com>
2024-11-11 13:19:25 +0100

    update embedding config

--- config/embedding_config.py
+++ config/embedding_config.py
@@ -1,11 +1,16 @@
 # this file containst default values related to embeddings and creating vectordb
+import os
 
-model = "all-MiniLM-L6-v2"  
+model_name = "sentence-transformers/all-mpnet-base-v2"  
 
-vector_db = "chromaDB"
+vector_db = "chromaDB" # Allowed Values ['chromaDB', 'FAISS']
 
-raw_db = "" 
+raw_db = "/home/ahmad-unibe/gutenberg_200"  #root directory to where raw documents are stored
 
-db_diretory = ""
+db_diretory = os.path.expanduser('~') + '/.db'
 
-chunk_size = 512
+chunk_size = 200
+
+tokenizer_model = "xlm-roberta-base"
+
+documents_type = 'txt'  #valid options: 'txt', 'pdf'
