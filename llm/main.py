acc5fbdd29bfdbc98a1a4d6897a3152dbb79028b
Ahmad Alhineidi <ahmadhineidi@hotmail.com>
2024-11-20 15:02:31 +0100

    add check function if model exist

--- llm/main.py
+++ llm/main.py
@@ -1,5 +1,6 @@
 import ollama
 
+
 class Responder:
     def __init__(self, data: str, model: str, prompt_template: str, query: str) -> None:
         self.data = data 
@@ -11,6 +12,7 @@ class Responder:
 
     
     def generate_response(self) -> str:
+        self._check_model()
         try:
             model_output = ollama.generate(model=self.model, prompt=self.prompt)
             return model_output['response']
@@ -18,3 +20,20 @@ class Responder:
             raise ValueError(f"Response does not contain expected key: {e}")
         except Exception as e:
             raise RuntimeError(f"An error occurred during response generation: {e}")
+        
+    def _check_model(self):
+        try:
+            models = ollama.list()['models']
+            model_names = [model['name'] for model in models]
+        except Exception as e:
+            raise RuntimeError(f"Failed to retrieve the list of models: {e}")
+
+        if self.model not in model_names:
+            print(f"Model '{self.model}' is not downloaded. Attempting to download...")
+            try:
+                ollama.pull(self.model)
+                print(f"Successfully downloaded model '{self.model}'.")
+            except ollama._types.ResponseError as e:
+                raise ValueError(f"Model '{self.model}' does not exist in the Ollama repository. Please check the model name.")
+            except Exception as e:
+                raise RuntimeError(f"An error occurred while downloading the model '{self.model}': {e}")
