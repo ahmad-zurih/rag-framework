bdab04c50cdbbb2cf0192ce6c4992ef19ca3e0e0
Ahmad Alhineidi <ahmadhineidi@hotmail.com>
2024-12-03 13:49:00 +0100

    remove document type variable

--- config/embedding_config.py
+++ config/embedding_config.py
@@ -5,14 +5,12 @@ model_name = "sentence-transformers/all-mpnet-base-v2"
 
 vector_db = "chromaDB" # Allowed Values ['chromaDB', 'FAISS']
 
-collection_name = "document_embeddings"
+collection_name = "dsl_embeddings"
 
-raw_db = "/home/ahmad-unibe/gutenberg_200"  #root directory to where raw documents are stored
+raw_db = "/mnt/c/Users/ahmad/OneDrive/Desktop/dsl_data"  #root directory to where raw documents are stored
 
 data_language = "english" #variable for the tokenizer. Supported language = ['czech', 'danish', 'dutch', 'english', 'estonian', 'finnish', 'french', 'german' ,'greek' ,'italian' ,'norwegian', 'polish' ,'portuguese', 'russian' ,'slovene','spanish', 'swedish', 'turkish']
 
 db_directory = os.path.join(os.path.expanduser('~'), '.db')  #default. Change it to where you want to store the vector DB
 
-chunk_size = 30
-
-documents_type = 'txt'  #valid options: 'txt', 'pdf'
+chunk_size = 20
