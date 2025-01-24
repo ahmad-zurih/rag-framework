35eba83dc512d804567628f962c9cee9d2126d10
Ahmad Alhineidi <ahmadhineidi@hotmail.com>
2025-01-24 13:45:07 +0100

    add generic configs

--- config/embedding_config.py
+++ config/embedding_config.py
@@ -1,16 +1,16 @@
 # this file containst default values related to embeddings and creating vectordb
 import os
 
-model_name = "sentence-transformers/all-mpnet-base-v2"  
+model_name = "Lajavaness/bilingual-embedding-large"  #choose any embedding model you prefer
 
-vector_db = "chromaDB" # Allowed Values ['chromaDB', 'FAISS']
+vector_db = "chromaDB" # Allowed Values ['chromaDB', 'FAISS']. Only ChromaDB works now
 
-collection_name = "dsl_embeddings"
+collection_name = "my_collection"
 
-raw_db = "/mnt/c/Users/ahmad/OneDrive/Desktop/dsl_data"  #root directory to where raw documents are stored
+raw_db = "/path/to/data"  #root directory to where raw documents are stored
 
 data_language = "english" #variable for the tokenizer. Supported language = ['czech', 'danish', 'dutch', 'english', 'estonian', 'finnish', 'french', 'german' ,'greek' ,'italian' ,'norwegian', 'polish' ,'portuguese', 'russian' ,'slovene','spanish', 'swedish', 'turkish']
 
 db_directory = os.path.join(os.path.expanduser('~'), '.db')  #default. Change it to where you want to store the vector DB
 
-chunk_size = 20
+chunk_size = 20   #number of sentences each chunk will contain in the vector db
--- config/llm_config.py
+++ config/llm_config.py
@@ -1,8 +1,8 @@
-llm_model = 'qwen2.5-coder:1.5b' # select any model available on the ollama site https://ollama.com/search
+llm_model = 'granite3.1-dense' # select any model available on the ollama site https://ollama.com/search
 
 
 prompt = """
-given the following data: {data}
-and the following query: {query}
-generate a responde. If there are no relevant information in the data, state that you don't have an answer
+You are a helpful polite assistat that works at the Data Science Lab (DSL). given the following data about DSL: \n {data} \n
+and the following query: \n{query}\n
+generate a responde. If there are no relevant information in the data, state that you don't have an answer and advise to contact DSL through info.dsl@unibe.ch or support.dsl@unibe.ch
 """
