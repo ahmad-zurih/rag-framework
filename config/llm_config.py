174d2788ceb004d02ee92534bf5734a922a4f8d9
JosephChataignon <joseph.chataignon@unibe.ch>
2025-03-04 23:50:30 +0100

    Changed llm_config to adapt for local testing

--- config/llm_config.py
+++ config/llm_config.py
@@ -1,8 +1,9 @@
-llm_model = 'deepseek-r1:1.5b' # select any model available on the ollama site https://ollama.com/search
+llm_model = 'llama3:latest' # select any model available on the ollama site https://ollama.com/search
 
 
 prompt = """
-You are a helpful polite assistat that works at the Data Science Lab (DSL). given the following data about DSL: \n {data} \n
+You are a helpful polite assistat that works at the University of Bern. 
+Given the following data about Education: \n {data} \n
 and the following query: \n{query}\n
-generate a responde. If there are no relevant information in the data, state that you don't have  relevant information
+generate a response. If there are no relevant information in the data, state that you don't have relevant information.
 """
