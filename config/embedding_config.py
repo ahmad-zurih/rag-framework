660cf05ab14a95f0671edbeefaae561be32f1e5e
JosephChataignon <joseph.chataignon@unibe.ch>
2025-03-05 15:37:45 +0100

    test plugging another embedding model

--- config/embedding_config.py
+++ config/embedding_config.py
@@ -1,7 +1,7 @@
 # this file containst default values related to embeddings and creating vectordb
 import os
 
-model_name = "Lajavaness/bilingual-embedding-large"  #choose any embedding model you prefer
+model_name = "sentence-transformers/all-mpnet-base-v2"  #choose any embedding model you prefer
 
 vector_db = "chromaDB" # Allowed Values ['chromaDB', 'FAISS']. Only ChromaDB works now
 
@@ -11,6 +11,6 @@ raw_db = "/home/joseph/Documents/eduscience_data_sample"  #root directory to whe
 
 data_language = "english" #variable for the tokenizer. Supported language = ['czech', 'danish', 'dutch', 'english', 'estonian', 'finnish', 'french', 'german' ,'greek' ,'italian' ,'norwegian', 'polish' ,'portuguese', 'russian' ,'slovene','spanish', 'swedish', 'turkish']
 
-db_directory = os.path.join(os.path.expanduser('~'), '.db')  #default. Change it to where you want to store the vector DB
+db_directory = os.path.join(os.path.expanduser('~'), '_MPNet_embeddings.db')  #default. Change it to where you want to store the vector DB
 
 chunk_size = 20   #number of sentences each chunk will contain in the vector db
